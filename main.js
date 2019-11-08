d64e4261e0fd4858f1e6a8e19d8cd38230492f94
elisainz <elisainz@yahoo.com.ar>
2019-11-08 19:02:31 -0300

    updated to SPA
    Single Page Application

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 function cambiarMes(mes) {
-  if(mes == "octubre"){
-    $("#septiembre" ).hide();
-  $("#octubre").show()
-  } else if(mes == "septiembre"){
-     $("#octubre").hide()
-    $("#septiembre" ).show();
- 
-  }
-  
-  window.onload = function() {
-    
-    $("#octubre").hide()
-    $("#septiembre" ).show();
-  }
-  
+	if (mes == "octubre") {
+		$("#septiembre").hide();
+		$("#octubre").show()
+	} else if (mes == "septiembre") {
+		$("#octubre").hide()
+		$("#septiembre").show();
+
+	}
+
 }
+
+
+
+function mostrarPagina(pagina) {
+	if (pagina == "home") {
+		$("#game-info").hide()
+		$("#contact").hide();
+		$("#home").show()
+	}
+
+	if (pagina == "game-info") {
+		$("#home").hide()
+		$("#contact").hide();
+		$("#game-info").show()
+	}
+
+	if (pagina == "contact") {
+		$("#home").hide()
+		$("#game-info").hide();
+		$("#contact").show()
+
+	}
+}
+
+
+	$("#octubre").hide()
+	$("#septiembre").show();
+
+	$("#home").hide()
+	$("#contact").hide()
+	$("#game-info").show();
+
+
